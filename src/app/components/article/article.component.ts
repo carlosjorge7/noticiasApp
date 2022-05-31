ccbc1c9bc5323b05d711c09f1a36c4e8f85d7fd5
carlosjorge7 <carlosjorge7@gmail.com>
2022-05-31 16:54:28 +0200

    componentes reusables in tab1. Input de datos

--- src/app/components/article/article.component.ts
+++ src/app/components/article/article.component.ts
@@ -1,15 +1,16 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Article } from 'src/app/interfaces';
 
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
   styleUrls: ['./article.component.scss'],
 })
-export class ArticleComponent implements OnInit {
+export class ArticleComponent {
 
+  @Input() article: Article;
+  @Input() index: number;
 
   constructor() { }
-
-  ngOnInit() {}
 
 }
--- src/app/components/articles/articles.component.ts
+++ src/app/components/articles/articles.component.ts
@@ -1,14 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Article } from 'src/app/interfaces';
 
 @Component({
   selector: 'app-articles',
   templateUrl: './articles.component.html',
   styleUrls: ['./articles.component.scss'],
 })
-export class ArticlesComponent implements OnInit {
+export class ArticlesComponent {
+
+  @Input() articles: Article[] = [];
 
   constructor() { }
 
-  ngOnInit() {}
+
 
 }
